712c0ae5fb8fb93bea83b3e567ef6fda5a6d74b8
Nahir <marescanahir18@gmail.com>
2023-05-15 12:43:53 -0300

    Footer
    Cambio de posicionamiento

--- finderservice/components/Footer.js
+++ finderservice/components/Footer.js
@@ -1,6 +1,7 @@
 export default function Footer() {
     return(
-        <section className="bg-slate-300 py-6">
+        
+        <section className="bg-slate-300 py-6" style={{ position: "fixed", bottom: 0, left: 0, width: "100%" }}>
             <div className="max-w-container h-full mx-auto font-titleFont">
                 <ul className="max-w-1/3 text-sm font-titleFont text-slate-700 grid grid-cols-3 gap-2 mt-6">
                     <li>Quiénes somos</li>
