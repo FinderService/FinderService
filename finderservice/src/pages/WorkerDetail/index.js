9b32bc940c230199ea7ef9abbd94451e859320cf
Oda2022 <odalismallqui@gmail.com>
2023-05-20 16:54:08 -0500

    payment do it

--- finderservice/src/pages/WorkerDetail/index.js
+++ finderservice/src/pages/WorkerDetail/index.js
@@ -3,8 +3,39 @@ import Layout from "@components/Layout";
 import { workers1 } from "@public/assets";
 import Image from "next/image"
 import Link from "next/link";
+import axios from "axios";
 
 export default function WorkerDetail() {
+
+  const handlePayClick = async () => {
+    try {
+      
+      const response = await axios.post("/api/payment", {
+        items: [
+          {
+            title: 'Worker',
+            quantity:1,
+            currency_id:'ARS',
+            unit_price:1000
+          }
+        ],
+        back_urls:{
+          success:'http://localhost:3000',
+          failure:'http://localhost:3000',
+          pending:'http://localhost:3000',
+        },
+        auto_return:'approved',
+        binary_mode:false,
+      });
+      const { init_point } = response.data;
+      // Redirige al usuario a la URL de pago proporcionada por `init_point`
+      window.location.href = init_point;
+    } catch (error) {
+      console.error("Error al crear la preferencia de pago:", error);
+    }
+  };
+
+
   return (
     <Layout>
         <div className="font-bold flex justify-center items-center mt-10">Detalle del profesional seleccionado</div>
@@ -18,9 +49,10 @@ export default function WorkerDetail() {
             <br></br>
             <p>Detalle de trabajador: Servicios de fontanería profesional con mas de cinco años de experiencia en el rubro.</p>
             <div class= "flex justify-center items-center space-x-8 mt-5 flex-row">
-                <Link href="/Payment">
-                <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">CONTRATAR</button>
-                </Link>
+                {/* <Link href="/Payment"> */}
+                <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+                        onClick={handlePayClick}>CONTRATAR</button>
+                {/* </Link> */}
                 <Link href="/MyOffers">
                 <button class="bg-amber-400 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded">Ver Servicios</button>
                 </Link>
