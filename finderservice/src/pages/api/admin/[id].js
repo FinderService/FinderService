df981b69e4dd6ef3e95b3c36793dfedd87fb9f24
Benny Wiliams <105727859+bennywr20@users.noreply.github.com>
2023-05-21 04:45:36 +0200

    RUTAS ADMIN
    RUTAS ADMIN

--- finderservice/src/pages/api/admin/[id].js
+++ finderservice/src/pages/api/admin/[id].js
@@ -1,35 +1,53 @@
 import Worker from "../../../models/Worker.js";
 import Employer from "../../../models/Employer.js";
+import Admin from "../../../models/Admin.js";
+import { getSession } from "next-auth/react";
 import { dbConnect, dbDisconnect } from "../../../utils/mongoose.js";
 
 export default async function handler(req, res) {
-  const { id } = req.query;
   const { method } = req;
   const { body } = req;
-  const model = body.category == "W" ? Worker : Employer;
+  const { id } = req.query;
+  const model = body.profile === "worker" ? Worker : Employer;
+  console.log(model)
+  const session = await getSession({ req });
+  const user = session === null ? "No user" : session.user.email;
 
   await dbConnect();
 
   switch (method) {
     case "PUT": {
       try {
+        const dataAdmin = await Admin.find({});
+        if (
+          !dataAdmin.some(
+            (x) => x.email === user[0].toUpperCase() + user.substring(1)
+          )
+        ) {
+          res.status(402).json({ authorization: "No Auth" });
+        }
+        const model = body.profile == "worker" ? Worker : Employer;
         const updateModel = await model.updateOne(
           { _id: id },
           {
             $set: body,
           }
         );
+        
         await dbDisconnect();
-        res.status(200).json({ status: "Changed", user: updateModel });
+        return res.status(200).json({ status: "Changed", user: updateModel });
       } catch (error) {
+        await dbDisconnect();
         return res.status(404).json({ status: "Error", err: errro });
       }
     }
     case "DELETE": {
       try {
-        await Model.findByIdAndDelete(id);
-        await dbDisconnect();
-        return res.status(200).json({ status: "Succes" });
+       await Worker.findByIdAndDelete(id);
+       await Employer.findByIdAndDelete(id);
+      await dbDisconnect();
+        
+        return res.status(200).json({ status: data });
       } catch (error) {
         await dbDisconnect();
         res.status(404).json({ error: "User Not Found!", numId: id });
--- finderservice/src/pages/api/admin/index.js
+++ finderservice/src/pages/api/admin/index.js
@@ -1,27 +1,35 @@
-
 import Worker from "../../../models/Worker.js";
 import Employer from "../../../models/Employer.js";
-import { dbConnect , dbDisconnect } from "@/utils/mongoose.js";
-import { converter } from "../../../utils/converter.js";
-
+import Admin from "../../../models/Admin.js";
+import { dbConnect, dbDisconnect } from "../../../utils/mongoose.js";
+import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
+  const session = await getSession({ req });
+  const user = session === null ? "No user" : session.user.email;
+
   await dbConnect();
   const { method } = req;
   switch (method) {
     case "GET": {
       try {
+        const dataAdmin = await Admin.find({});
+        if (
+          !dataAdmin.some(
+            (x) => x.email === user[0].toUpperCase() + user.substring(1)
+          )
+        ) {
+          res.status(402).json({ authorization: "No Auth" });
+        }
+
         const dataWorker = await Worker.find({});
         const dataEmployer = await Employer.find({});
         await dbDisconnect();
-        const data = [
-          ...converter(dataWorker, "W"),
-          ...converter(dataEmployer, "E"),
-        ];
+        const data = [...dataWorker, ...dataEmployer];
         if (data.length == 0)
           return res
             .status(400)
-            .json({sta:"hola"})
+            .json({ Status: "No existen los Worker , tampoco los Employers" });
         return res.status(200).json(data);
       } catch {
         return res
@@ -29,9 +37,8 @@ export default async function handler(req, res) {
           .json({ error: "Algo salio mal al momento de hacer la peticion" });
       }
     }
-    case "POST": {
-      await dbDisconnect();
-      res.json({ POST: "succees" });
+    default: {
+      return res.status(404).json({ method: "Not Found" });
     }
   }
 }
