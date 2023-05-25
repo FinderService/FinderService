df4536bd3e46e84bb551fb37826d242409956d44
Nicolas Pua <nicopua7@gmail.com>
2023-05-25 17:31:41 -0300

    /HomeAdmin, UsersAdmin, UsersComments created

--- finderservice/src/pages/HomeWorker/[id].js
+++ finderservice/src/pages/HomeWorker/[id].js
@@ -6,11 +6,13 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { loader } from '@public/assets';
 import Link from "next/link";
+import { useUser } from "@context/UserContext";
 
 export default function WorkDetail() {
     const { workDetail, setWorkDetail, getJobReqs} = useWorkers();
     const router = useRouter();
     const { id } = router.query;
+    const { userData } = useUser();
 
     useEffect(()=>{
         getJobReqs(id);
@@ -18,11 +20,18 @@ export default function WorkDetail() {
         //eslint-disable-next-line
     },[])
 
+    const sendWorkInfo = () =>{
+        const { name, description, date, photo, type , address, employer} = workDetail;
+
+        localStorage.setItem('workInfo',JSON.stringify({ name, description, date, photo, type , address, employer}));
+        localStorage.setItem('userInfo',JSON.stringify(userData));
+    }
+
     return (
         <Layout>
             {workDetail.name? <>
             <div className="font-bold text-3xl flex justify-center items-center mt-10">Detalle de oferta seleccionada</div>
-            <div class="flex justify-center items-center mt-10 ">
+            <div className="flex justify-center items-center mt-10 ">
                 <div class="flex justify-around bg-white shadow-2xl rounded px-8 pt-6 pb-8 mb-10">
                     <Image className="mr-10 ml-5" src={workDetail.photo} width={300} height={300} alt="workDetail"/>
                     <div className="flex flex-col justify-around">
@@ -32,9 +41,9 @@ export default function WorkDetail() {
                         <p className="mb-2">Fecha: {workDetail.date.slice(0,10)}</p>
                         <p className="mb-2">Destinado para: {workDetail.type.map((tipo)=> tipo.name).toString()}</p>
                         <p className="mb-2">Publicado por {workDetail.employer}</p>
-                        <div class= "flex justify-center items-center space-x-8 mt-5">
-                            <Link href="/jobpostulations"><button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">Postularme</button></Link>
-                            <Link href="/HomeWorker"><button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded">Volver</button></Link>
+                        <div className= "flex justify-center items-center space-x-8 mt-5">
+                            <Link href="/jobpostulations" onClick={sendWorkInfo}><button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">Postularme</button></Link>
+                            <Link href="/HomeWorker"><button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded">Volver</button></Link>
                         </div>
                     </div>
                 </div>
